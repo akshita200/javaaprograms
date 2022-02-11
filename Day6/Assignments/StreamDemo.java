62a748088142ff02e6c0636b48db00057c66cd21
akshita <akshita6763@gmail.com>
2022-02-11 19:41:54 +0530

    Assignments

---
+++ Day6/Assignments/StreamDemo.java
@@ -0,0 +1,14 @@
+package task;
+import java.util.stream.Stream;
+
+public class StreamDemo {
+	
+		public static void  main(String ar[]) {
+			//functional style programming
+		Stream.iterate(1,element ->element+1).filter(element ->element % 5==0).limit(3).forEach(System.out::println);
+			
+		}
+
+	}
+
+
