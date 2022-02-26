d74e3604e240489d33ec8c7e4e675f295f264cbe
akshita <akshita6763@gmail.com>
2022-02-26 21:20:44 +0530

    HibernatePrograms

---
+++ Day3/concurrent/GenericsDemo.java
@@ -0,0 +1,23 @@
+package task;
+import java.util.ArrayList;
+
+public class GenericsDemo {
+	
+
+	//import java.util.ArrayList;
+
+	
+
+		public static void main(String ar[]) {
+
+			ArrayList<String> k = new ArrayList(); // raw type
+
+			k.add("12"); // integer
+			k.add("45.6"); // double
+			k.add("ram"); // string based
+		}
+		
+
+	}
+
+
---
+++ Hibernateprograms/Ecommerce1/src/main/java/com/main/Main.java
@@ -0,0 +1,18 @@
+package com.main;
+
+
+	import java.util.Iterator;
+	import java.util.List;
+	import com.ecomm.Product;
+	import com.ecomm.Rating;
+	import com.ecomm.businesslogic.service;
+	public class Main {
+		public static void main(String args[]) {
+			
+			
+			
+			
+			
+			service serve = new service();
+		}
+}
